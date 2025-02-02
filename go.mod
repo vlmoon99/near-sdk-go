968b07f83815cd3b19a39f2d99b9182ed26e1630
vlmoon99 <vlmoon@proton.me>
2025-02-02 13:59:38 +0000

    (feature) : improve build size

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module sdk
 
 go 1.23.5
+
+require github.com/mr-tron/base58 v1.2.0
--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"strconv"
+
+	"github.com/mr-tron/base58"
+)
+
 // 0.Create structs
 type Person struct {
 	Name  string
@@ -16,39 +24,44 @@ func helloworld() {
 	}
 	LogString("Detected current : " + inputType)
 	LogString(string(input))
-
 	//Basic Serialization into bytes
 	parser := NewParser(input)
 	LogString("Parsed Data:" + string(parser.data))
 	//Use parser.GetType(GetRawBytes,GetString,GetInt ... etc) for getting the correct type from json
-
 	name, nameErr := parser.GetRawBytes("name")
 	if nameErr != nil {
 		LogString("Error while getting name key")
 	}
-	LogString("name:" + (string(name)))
+	LogString("name:" + string(name))
+	LogString("name hex:" + hex.EncodeToString(name))
+	LogString("name base64:" + base64.RawStdEncoding.EncodeToString(name))
+	LogString("name base58:" + base58.Encode(name))
 
 	//Save raw bytes into storage
 	key := []byte("person")
-	ContractStorageWrite(key, parser.data)
+	result, writeErr := ContractStorageWrite(key, parser.data)
+	if writeErr != nil {
+		LogString("Error while write to the name key")
+	}
+	LogString("result:" + strconv.FormatBool(result))
+
 	//Read
 	data, readErr := ContractStorageRead(key)
-	LogString("Write Data:" + (string(data)))
+	LogString("Write Data:" + string(data))
 
 	if readErr != nil {
 		LogString("Error while reading name key")
 	}
-	LogString("Read Data:" + (string(data)))
+	LogString("Read Data:" + string(data))
 
 	//Deserialize
 	deserializeParser := NewParser(data)
-	LogString("Read Data:" + (string(deserializeParser.data)))
-
+	LogString("Deserialize Data:" + string(deserializeParser.data))
 	age, ageErr := parser.GetRawBytes("age")
 	if ageErr != nil {
 		LogString("Error while getting age key")
 	}
-	LogString("age:" + (string(age)))
+	LogString("age:" + string(age))
 
 	//Return Result
 	ContractValueReturn(age)
